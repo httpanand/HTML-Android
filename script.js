fb5cf9eb14767a8ef71eaae7a2d5c9498de39332
Anand <87514488+httpanand@users.noreply.github.com>
2021-10-07 21:16:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let d = today.getDate();
+  
+  m = checkTime(m);
+  document.getElementById('time').innerHTML =  h + ":" + m;
+  setTimeout(startTime, 1000);
+  document.getElementById('time2').innerHTML = h + ":" + m;
+  document.getElementById('date').innerHTML = d + " October " + "2021";
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  
+  return i;
+}
